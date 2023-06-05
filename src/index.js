cbdd3c461a86d6b0be4692e99b1d717aa1e88761
Jhojan1022 <86871577+Jhojan1022@users.noreply.github.com>
2023-06-04 21:34:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ app.all("/iniciarActividad", actividadesController.iniciarActividad)
 app.all("/finalizarActividad", actividadesController.finalizarActividad)
 app.all("/cerrarActividades", actividadesController.cerrarActividades)
 
-const server = app.listen(port  || process.env.port, () => {
+const server = app.listen(process.env.port, () => {
     server.timeout = 30000; // Establece el tiempo de espera a 30 segundos
     console.log("Servidor en puerto " + port);
   });
